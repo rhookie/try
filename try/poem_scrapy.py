b9bca3ff251bf516d3d7ce6b0156dd70be6eeb5c
rhookie <1324587720@qq.com>
2020-12-13 11:11:07 +0800

    scrapy poem

---
+++ try/poem_scrapy.py
@@ -0,0 +1,26 @@
+import scrapy
+
+class PoemsSpider(scrapy.Spider):
+	name='peoms'
+    allowed_domains=['diwany.org']
+    start_urls=['http://diwany.org/']
+    
+    def parse(self,response):
+        bahrs_links=response.xpath("//div[@class='menu-behers-ar-container']//a")
+        for link in bahrs_links:
+            yield response.follow(link,callback=self.parse_bahr,cb_kwargs=dict(poems_links=list()))
+    
+    def parse_bahr(self,respone,poems_links):
+        print('the initial number of poems links for the url:',response.url,'is',len(poems_links))
+        poems_links+=respone.xpath("//div[@class='post hentry ivycat-post']/h2//a")
+        next_page=response.css(".pip-nav-next a")
+        if next_page:
+            yield response.follow(
+                next_page[0],
+                callback=self.parse_bahr,
+                cb_kwargs=dict(poems_links=poems_links),
+                )
+        else:
+            yield from respone.follow_all(
+                poems_links,callback=self.parse_poem,)
+              
